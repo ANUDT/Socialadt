37270140b9532e38f0aea16537bc2904419996ad
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-16 10:43:50 +0100

    reaction js result boidy

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,3 +1,30 @@
 //reaction.js is to build schema for reaction in database Mongoose in MongoDB
 //Types, schema and mongoose are teh parts we use to build the the models
-const {Schema,Types} = require('mongoose');
+const { Schema, Types } = require('mongoose');
+
+const resultSchema = new Schema({
+  resultId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  resultBody: {
+    type: String,
+    required: true,
+    maxlength:200,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+}, {
+  toJSON: {
+    getters: true,
+  },
+  id: false,
+});
+
