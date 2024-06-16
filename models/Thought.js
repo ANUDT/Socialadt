ac465cd304be746500ab082720887c9aceadb1c4
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-16 14:54:56 +0100

    Thought.js models created

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const resultSchema = require('./Reaction');
+
+const resultSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 200,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+}, {
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+  id: false,
+});
+
+resultSchema.virtual('resultCount').get(function () {
+  return this.results.length;
+});
+
+const Thought = model('Thought', resultSchema);
+
+module.exports = Thought;
