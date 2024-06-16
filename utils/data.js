8c21d36c5ca5a14df07195f84c1ab6f610f5d235
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-16 16:43:54 +0100

    utils/data.js thoughts and users added

--- utils/data.js
+++ utils/data.js
@@ -13,19 +13,89 @@ const users = [
       thoughts: [],
       friends: [],
     },
-  ];
-  
-  const thoughts = [
-    {
-      thoughtText: 'This is posted by Pipi',
-      username: 'pipi_langkous',
+
+   {
+    username: 'mini_mouse',
+    email: 'mini_mouse@example.com',
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: 'daffy_duck',
+    email: 'daffy_duck@example.com',
+    thoughts: [],
+    friends: [],
+  },
+
+  {
+    username: 'shrek_green',
+    email: 'shrek_green@example.com',
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: 'hilly_billy',
+    email: 'hilly_billy@example.com',
+    thoughts: [],
+    friends: [],
+  },
+
+ {
+  username: 'top_cat',
+  email: 'top_cat@example.com',
+  thoughts: [],
+  friends: [],
+},
+{
+  username: 'king_kong',
+  email: 'king_kong@example.com',
+  thoughts: [],
+  friends: [],
+},
+];
+
+const thoughts = [
+  {
+    thoughtText: 'This is posted by Pipi',
+    username: 'pipi_langkous',
+    results: [],
+  },
+  {
+    thoughtText: 'Tom has send you a message',
+    username: 'tom_jerry',
+    results: [],
+  },
+  {
+      thoughtText: 'This is posted by Mini',
+      username: 'mini_mouses',
       results: [],
     },
     {
-      thoughtText: 'Tom has send you a message',
-      username: 'tom_jerry',
+      thoughtText: 'Daffy has send you a message',
+      username: 'daffy_duck',
       results: [],
     },
-  ];
+    {
+      thoughtText: 'This is posted by Shrek',
+      username: 'shrek_green',
+      results: [],
+    },
+    {
+      thoughtText: 'Hilly Billy has send you a message',
+      username: 'hilly_billy',
+      results: [],
+    },
+    {
+        thoughtText: 'This is posted by Top Cat',
+        username: 'top_cat',
+        results: [],
+      },
+      {
+        thoughtText: 'King Kong has send you a message',
+        username: 'king_kong',
+        results: [],
+      },
+];
+
   
   module.exports = { users, thoughts };
