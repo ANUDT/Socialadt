627512e4a5f81d40071d43df51a1f9af89af1f51
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-16 15:46:32 +0100

    fixed connection strings

--- Backend/Config/Connection.js
+++ Backend/Config/Connection.js
@@ -1,7 +1,7 @@
 //Example connection.js from mini project
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb_URI=mongodb://localhost:27017/week-18-social-api'
+const connectionString = 'mongodb://localhost:27017/week-18-social-api'
 
 connect(connectionString);
 
