89fd94a3cb760dcb7d11b60619d69a7e30bba23e
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-16 10:43:30 +0100

    result schema build

---
+++ models/Reaction.js
@@ -0,0 +1,3 @@
+//reaction.js is to build schema for reaction in database Mongoose in MongoDB
+//Types, schema and mongoose are teh parts we use to build the the models
+const {Schema,Types} = require('mongoose');
